b01356958027d857ca74cd9a12eb99b5c39f6d47
Siddhartha <siddharthamukherjee0709@gmail.com>
2025-09-04 11:08:43 +0530

    cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors('*'));
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Banking Application API');
